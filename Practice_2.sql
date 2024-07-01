a39b14b7f9177192849fe9ab42a0a64c84f32b37
Lê Nguyễn Thảo Chi <167676113+1stKei@users.noreply.github.com>
2024-07-01 21:24:46 +0700

    Update Practice_2.sql

--- Practice_2.sql
+++ Practice_2.sql
@@ -28,23 +28,31 @@ MAX(issued_amount) - MIN(issued_amount) AS difference FROM monthly_cards_issued
 GROUP BY card_name
 ORDER BY MAX(issued_amount) - MIN(issued_amount) DESC;
 
---- nháp
+--- ex8: datalemur-non-profitable-drugs
 SELECT manufacturer,
 COUNT(drug) AS drug_count,
-abs(sum(total_sales - cogs)) AS total_loss
-FROM pharmacy_sales
-  
-GROUP BY manufacturer
-HAVING total_sales - cogs < 0
-ORDER BY abs(total_sales - cogs) DESC
-
-
-
---- đúng
-SELECT manufacturer,
-COUNT(drug) AS drug_count,
-abs(sum(total_sales - cogs)) as total_loss FROM pharmacy_sales
-  
+abs(sum(total_sales - cogs)) as total_loss FROM pharmacy_sales  
 WHERE total_sales < cogs
 GROUP BY manufacturer
 ORDER BY total_loss DESC
+
+--- ex9: leetcode-not-boring-movies
+select * from cinema
+where ID%2 != 0 and description != 'boring'
+order by rating desc
+
+--- ex10: leetcode-number-of-unique-subject
+select teacher_id,
+count(distinct subject_id) as cnt from Teacher
+group by teacher_id
+
+--- ex11: leetcode-find-followers-count
+select user_id,
+count(follower_id) as followers_count from Followers
+group by user_id
+order by user_id 
+
+--- ex12:leetcode-classes-more-than-5-students.
+select class from Courses 
+group by class
+having count(student) >= 5
