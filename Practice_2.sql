3d534238cd64c34590f4d8918d37bd6c1ca93c57
Lê Nguyễn Thảo Chi <167676113+1stKei@users.noreply.github.com>
2024-07-01 17:06:50 +0700

    Update Practice_2.sql

--- Practice_2.sql
+++ Practice_2.sql
@@ -6,3 +6,11 @@ select count(city) - count(distinct city) from station
 
 --- ex3: hackerrank-the-blunder
 select ceiling (avg(salary) - avg(replace (salary, 0, ''))) from employees
+
+--- ex4: datalemur-alibaba-compressed-mean
+SELECT ROUND(SUM(item_count::DECIMAL*order_occurrences)/SUM(order_occurrences),1) AS mean FROM items_per_order
+
+--- ex5: datalemur-matching-skills
+SELECT candidate_id FROM candidates WHERE skill IN ('Python', 'Tableau', 'PostgreSQL') 
+GROUP BY candidate_id HAVING COUNT(skill) = 3
+ORDER BY candidate_id;
