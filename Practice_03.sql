64a4ce302a7ad57925d1bef829596be9edad00d5
Lê Nguyễn Thảo Chi <167676113+1stKei@users.noreply.github.com>
2024-07-04 17:15:02 +0700

    Update Practice_03.sql

--- Practice_03.sql
+++ Practice_03.sql
@@ -4,3 +4,13 @@ where marks > 75
 order by right(name,3), id
 
 --- ex2: leetcode-fix-names-in-a-table
+select user_id, concat(upper(left(name,1)), lower(substring(name, 2))) as name
+from Users
+order by user_id
+
+--- ex3: datalemur-total-drugs-sales
+SELECT manufacturer,
+'$' || round(SUM(total_sales)/1000000,0) ||' ' || 'million' AS sale 
+FROM pharmacy_sales
+GROUP BY(manufacturer)
+ORDER BY SUM(total_sales) DESC, manufacturer
