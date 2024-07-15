2f2c660f1d85b78b09494aa63895719eefc9b3f2
Lê Nguyễn Thảo Chi <167676113+1stKei@users.noreply.github.com>
2024-07-15 22:18:54 +0700

    Update Practice_05.sql

--- Practice_05.sql
+++ Practice_05.sql
@@ -31,3 +31,34 @@ LEFT JOIN products
 ON customer_contracts.product_id = products.product_id
 GROUP BY customer_contracts.customer_id
 HAVING COUNT(DISTINCT products.product_category) = 3 
+
+---ex5: leetcode-the-number-of-employees-which-report-to-each-employee.
+SELECT e1.employee_id, 
+       e1.NAME, 
+       Count(e2.reports_to)  AS reports_count, 
+       Round(Avg(e2.age), 0) AS average_age 
+FROM   employees e1 
+       INNER JOIN employees e2 
+               ON e1.employee_id = e2.reports_to 
+GROUP  BY e1.employee_id, 
+          e1.NAME 
+ORDER  BY 1 ASC
+
+---ex6: leetcode-list-the-products-ordered-in-a-period.
+    --- Không hiểu câu này
+SELECT
+    p.product_name,
+    SUM(o.unit) [unit]
+FROM Products p JOIN Orders o ON p.product_id = o.product_id
+WHERE MONTH(o.order_date) = 2
+    AND YEAR(o.order_date) = 2020
+GROUP BY
+    p.product_name
+HAVING SUM(o.unit) >= 100
+
+---ex7: leetcode-sql-page-with-no-likes.
+SELECT pages.page_id
+FROM pages
+LEFT OUTER JOIN page_likes AS likes
+  ON pages.page_id = likes.page_id
+WHERE likes.page_id IS NULL;
