0ed6c5c756169bd80b46e717079ea88b0a349f9a
Lê Nguyễn Thảo Chi <167676113+1stKei@users.noreply.github.com>
2024-07-19 10:38:53 +0700

    Update Practice_06.sql

--- Practice_06.sql
+++ Practice_06.sql
@@ -8,14 +8,15 @@ select
   description,
   COUNT(job_id) AS job_count
 FROM job_listings 
-GROUP BY company_id, title, description)
+GROUP BY company_id, title, description
+  )
   ---B2: trả lại kết quả số lượng job trùng
 SELECT count(job_count) FROM cte_job_count
 WHERE job_count >= 2
 
 
 --- ex2: datalemur-highest-grossing.
---- tạo bảng có rank
+  --- tạo bảng có rank
 WITH cte_spend_rank AS (
   SELECT category, product,
     sum(spend) AS total_spend,
@@ -24,8 +25,21 @@ WITH cte_spend_rank AS (
   WHERE EXTRACT(year FROM transaction_date) = 2022
   GROUP BY category, product
 )
-
---- category, product, and total spend có rank <=2
+  --- category, product, and total spend có rank <=2
 SELECT category, product, total_spend 
 from cte_spend_rank
 where spend_rank<= 2
+
+--- ex3: datalemur-frequent-callers.
+  --- tạo bảng để đếm số cuộc gọi của từng member > 3 cuộc
+WITH cte_case_id_count AS (
+SELECT policy_holder_id, 
+  COUNT(case_id) as case_id_count
+FROM callers
+GROUP BY policy_holder_id
+HAVING COUNT(case_id) >= 3
+)
+  ---đếm
+select count(*) as policy_holder_count from cte_case_id_count
+
+
