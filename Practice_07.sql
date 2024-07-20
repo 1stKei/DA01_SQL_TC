7f1a9838a308e2f25cfa9d527c8f81b97b9f622c
Lê Nguyễn Thảo Chi <167676113+1stKei@users.noreply.github.com>
2024-07-20 17:44:32 +0700

    Update Practice_07.sql

--- Practice_07.sql
+++ Practice_07.sql
@@ -1,1 +1,8 @@
 --- ex1: datalemur-yoy-growth-rate.
+SELECT EXTRACT(year FROM transaction_date) as year, 
+product_id,
+spend as curr_year_spend,
+lag(spend) OVER(PARTITION BY product_id ORDER BY transaction_date) as prev_year_spend,
+round((spend - lag(spend) OVER(PARTITION BY product_id ORDER BY transaction_date))
+/lag(spend) OVER(PARTITION BY product_id ORDER BY transaction_date) *100, 2)
+FROM user_transactions;
