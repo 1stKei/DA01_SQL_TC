fea854104e3c371dca844c6f7bd4b503dc57ce2d
Lê Nguyễn Thảo Chi <167676113+1stKei@users.noreply.github.com>
2024-07-28 16:10:02 +0700

    Update Project_02.sql

--- Project_02.sql
+++ Project_02.sql
@@ -22,3 +22,28 @@ order by 1
 select month_year, distinct_users,
 round(sum_sale_price/ count_order_id,2) as average_order_value
 from cte_2
+
+
+--- 4.Top 5 sản phẩm mỗi tháng.
+with cte_4_1 as (
+select
+  FORMAT_DATE('%Y-%m', created_at) as month_year,
+  order_items.product_id as product_id,
+  products.name as product_name,
+  sum(order_items.sale_price) as sales,
+  sum(products.cost) as cost, 
+  sum(order_items.sale_price) - sum(products.cost) as profit
+from bigquery-public-data.thelook_ecommerce.order_items as order_items
+join bigquery-public-data.thelook_ecommerce.products as products 
+on order_items.product_id = products.id
+group by FORMAT_DATE('%Y-%m', created_at), products.name, order_items.product_id
+order by 1
+),
+  --- Tạo cte để rank
+cte_4_2 as (
+select *,
+dense_rank() over (partition by month_year order by profit) as rank_per_month
+ from cte_4_1
+)
+select * from cte_4_2 
+where rank_per_month <=5
